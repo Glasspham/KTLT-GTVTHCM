f0e74895eec64cd8209198f9088f88e330de1671
Glasspham <95564442+Glasspham@users.noreply.github.com>
2024-02-26 16:23:53 +0700

    Update main.cpp

--- Lab1/Bai_11/main.cpp
+++ Lab1/Bai_11/main.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<math.h>
 using namespace std;
-
+#if 1
 struct point
 {
     double x, y, z;
@@ -18,3 +18,18 @@ int main()
     cin >> a >> b >> c >> d;
     cout << "Khoang cach tu A den mp (P): " << abs(a * A.x + b * A.y + c * A.z + d) / sqrt(pow(a,2) + pow(b,2) + pow(c,2)) << endl;
 }
+#endif
+
+#if 1
+int main()
+{
+    double Ax, Ay, Az;
+    cout << "Nhap toa do diem A(x,y,z): ";
+    cin >> Ax >> Ay >> Az;
+    double a, b, c, d;
+    cout << "Nhap cac he so mp(P): ";
+    cin >> a >> b >> c >> d;
+    cout << "Khoang cach tu A den mp (P): " << abs(a * Ax + b * Ay + c * Az + d) / sqrt(pow(a,2) + pow(b,2) + pow(c,2)) << endl;
+    return 0;
+}
+#endif
