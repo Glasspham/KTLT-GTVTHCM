908bb572f24703a5998409cb028351fcc8ce1a34
Glasspham <95564442+Glasspham@users.noreply.github.com>
2024-03-01 11:15:59 +0700

    Update main.cpp

--- Lab1/Bai_7/main.cpp
+++ Lab1/Bai_7/main.cpp
@@ -38,26 +38,30 @@ int main()
 #if 1
 int main() 
 {
-    double Ax, Ay, Bx, By, Cx, Cy;
-    cout << "Nhap vao toa do A(x,y): ";
-    cin >> Ax >> Ay;
-    cout << "Nhap vao toa do B(x,y): ";
-    cin >> Bx >> By;
-    cout << "Nhap vao toa do C(x,y): ";
-    cin >> Cx >> Cy;
-    
-    double lenAB = sqrt(pow(Bx - Ax, 2) + pow(By - Ay, 2));
-    double lenAC = sqrt(pow(Cx - Ax, 2) + pow(Cy - Ay, 2));
-    double lenBC = sqrt(pow(Cx - Bx, 2) + pow(Cy - By, 2));
-    
-    if(lenAB > 0 && lenAC > 0 && lenBC > 0 && lenAB + lenAC > lenBC && lenAB + lenBC > lenAC && lenAC + lenBC > lenAB) 
+    int choice;
+    do
     {
-        double p = (lenAB + lenAC + lenBC) / 2;
-        cout << "Chu vi tam giac ABC: " << 2 * p << endl;
-        cout << "Dien tich tam giac ABC: " << sqrt(p * (p - lenAB) * (p - lenAC) * (p - lenBC));
-    }
-    else
-        cout << "Khong ton tai tam gia ABC!";
+        double Ax, Ay, Bx, By, Cx, Cy;
+        cout << "Nhap vao toa do A(x,y): ";
+        cin >> Ax >> Ay;
+        cout << "Nhap vao toa do B(x,y): ";
+        cin >> Bx >> By;
+        cout << "Nhap vao toa do C(x,y): ";
+        cin >> Cx >> Cy;
+        double lenAB = sqrt(pow(Bx - Ax, 2) + pow(By - Ay, 2));
+        double lenAC = sqrt(pow(Cx - Ax, 2) + pow(Cy - Ay, 2));
+        double lenBC = sqrt(pow(Cx - Bx, 2) + pow(Cy - By, 2));
+        if(lenAB > 0 && lenAC > 0 && lenBC > 0 && lenAB + lenAC > lenBC && lenAB + lenBC > lenAC && lenAC + lenBC > lenAB) 
+        {
+            double p = (lenAB + lenAC + lenBC) / 2;
+            cout << "Chu vi tam giac ABC: " << 2 * p << endl;
+            cout << "Dien tich tam giac ABC: " << sqrt(p * (p - lenAB) * (p - lenAC) * (p - lenBC)) << endl;
+        }
+        else
+            cout << "Khong ton tai tam gia ABC! \n";
+        cout << "Ban co muon tinh tiep khong (1:co / 0:khong): ";
+        cin >> choice;
+    } while (choice == 1);
     return 0;
 }
 #endif
