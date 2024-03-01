cbb8b4341c4ce0b03c9b5a0ba7f87c578b3636e5
Glasspham <95564442+Glasspham@users.noreply.github.com>
2024-03-01 11:16:37 +0700

    Update main.cpp

--- Lab1/Bai_8/main.cpp
+++ Lab1/Bai_8/main.cpp
@@ -2,10 +2,24 @@
 using namespace std;
 int main()
 {
-    double km;
-    cin >> km;
-    if(km < 2)
-        cout << km * 15000 * (1 + 0.1);
-    else
-        cout << (15000 + (km - 1) * 12000) * (1 + 0.1);
+    int choice;
+    do
+    {
+        int km;
+        do
+        {
+            cout << "Nhap so km di duoc: ";
+            cin >> km;
+            if(km < 0) cout << "Nhap sai! \n";
+        } while (km < 0);
+        cout << "So tien la: ";
+        if(km == 0) cout << 0 << endl;
+        if(km == 1)
+            cout << 15000 * (1 + 0.1) << endl;
+        else
+            cout << (15000 + (km - 1) * 12000) * (1 + 0.1) << endl;
+        cout << "Ban co muon tinh tiep khong (1:co / 0:khong): ";
+        cin >> choice;
+    } while (choice == 1);
+    return 0;
 }
