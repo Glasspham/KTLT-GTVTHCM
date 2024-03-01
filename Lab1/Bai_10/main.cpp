d6e77b09888f2583db99a345d3a604a34d13942f
Glasspham <95564442+Glasspham@users.noreply.github.com>
2024-03-01 11:17:55 +0700

    Update main.cpp

--- Lab1/Bai_10/main.cpp
+++ Lab1/Bai_10/main.cpp
@@ -3,9 +3,21 @@
 using namespace std;
 int main()
 {
-    double a;
-    cout << "Chieu dai 1 canh hinh vuong: "; cin >> a;
-    cout << "Chu vi hinh vuong: " << 2 * a << endl;
-    cout << "Dien tich hinh vuong: " << a * a << endl;
-    cout << "Do dai duong cheo hinh vuong: " << sqrt(2) * a << endl;
+    int choice;
+    do
+    {
+        double a;
+        do
+        {
+            cout << "Chieu dai 1 canh hinh vuong: ";
+            cin >> a;
+            if(a < 0) cout << "Nhap sai! \n";
+        } while (a < 0);
+        cout << "Chu vi hinh vuong: " << 2 * a << endl;
+        cout << "Dien tich hinh vuong: " << a * a << endl;
+        cout << "Do dai duong cheo hinh vuong: " << sqrt(2) * a << endl;
+        cout << "Ban co muon tinh tiep khong (1:co / 0:khong): ";
+        cin >> choice;
+    } while (choice == 1);
+    return 0;
 }
